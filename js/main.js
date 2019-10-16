f76e371c53b0a38e5b0e5c14fc9664d53540f831
Vinicius Negrao de Menezes <vnsn_@outlook.com>
2019-10-16 16:56:56 -0300

    Feat: Inserting Projects section (not finished)

--- js/main.js
+++ js/main.js
@@ -1,11 +1,19 @@
 var imported = document.createElement('script');
 imported.src = 'js/modules/data/data.js';
 document.head.appendChild(imported);
+/**
+ * data object importado como 'data'.
+ * Modulos nao foram usados por que nao funcionam localmente devido
+ * a politica de Croos Origin CORS do Chrome.
+ */
 
+/**
+ * Event Listeners
+ */
 let chartsArray = document.querySelectorAll('.chartLink');
 
 chartsArray.forEach(function(elem) {
-	elem.addEventListener('click', function(event) {
+	elem.addEventListener('mouseenter', function(event) {
 		const caller = event.toElement.innerHTML;
 		switch (caller) {
 			case 'HTML':
@@ -27,17 +35,38 @@ chartsArray.forEach(function(elem) {
 	});
 });
 
+let infoArray = document.querySelectorAll('.projectLink');
+
+infoArray.forEach(function(elem) {
+	elem.addEventListener('mouseenter', function(event) {
+		const caller = event.toElement.innerHTML;
+		switch (caller) {
+			case 'WEBSITES':
+				dataChange(caller);
+				break;
+			case 'IONIC APPS':
+				dataChange(caller);
+				break;
+			case 'ANGULAR':
+				dataChange(caller);
+				break;
+		}
+	});
+});
+
 function buildChart(caller) {
 	const options = {
 		tooltips: {
 			enabled: false
 		}
 	};
-	var chartDiv = document.getElementById('chart');
+	let chartDiv = document.getElementById('chart');
 	chartDiv.innerHTML = '';
-	var chart = new Chart(chartDiv, {
+	const chart = new Chart(chartDiv, {
 		type: 'pie',
 		data: caller,
 		options: options
 	});
 }
+
+function dataChange(caller) {}
